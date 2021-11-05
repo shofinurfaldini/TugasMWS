637c0d26fd4f66d6764f92b730bd61af33d0a1c8
Shofi Isnaeni Nurfaldini <shofinurfaldini@gmail.com>
2021-11-05 22:53:14 +0700

    Initial commit

---
+++ app/src/main/java/com/mws/tugasmws/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mws.tugasmws
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnLogin : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, Dashboard :: class.java)
+        startActivity(intent)
+    }
+}
