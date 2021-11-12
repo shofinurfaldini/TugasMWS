668f049e768fa52be66ba9e3f7b3f7f4903ec681
Shofi Isnaeni Nurfaldini <shofinurfaldini@gmail.com>
2021-11-12 18:17:16 +0700

    Membuat Halaman Baru

--- app/src/main/java/com/mws/tugasmws/Dashboard.kt
+++ app/src/main/java/com/mws/tugasmws/Dashboard.kt
@@ -1,11 +1,26 @@
 package com.mws.tugasmws
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
-class Dashboard : AppCompatActivity() {
+class Dashboard : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var ig_tshirt1 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        ig_tshirt1 = findViewById(R.id.ig_tshirt1)
+        ig_tshirt1.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, Detail :: class.java)
+        startActivity(intent)
     }
 }
